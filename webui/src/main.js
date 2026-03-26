cfe8440e0d619471748b2919042e5e91d19dc240
iiishop <30495867+iiishop@users.noreply.github.com>
2026-03-26 10:31:37 +0000

    refactor: reset repository to blank baseline for rebuild

--- webui/src/main.js
+++
@@ -1,7 +0,0 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import './styles.css'
-import '@vue-flow/core/dist/style.css'
-import '@vue-flow/core/dist/theme-default.css'
-
-createApp(App).mount('#app')
